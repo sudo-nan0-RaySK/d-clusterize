ca49a89c152c52fbede39df104a8dc654725cdf1
sudo-nan0-RaySK <saksham.sethi2017@vitstudent.ac.in>
2018-12-29 15:44:28 +0530

    improved blocking

--- background.js
+++ background.js
@@ -14,6 +14,18 @@ chrome.webRequest.onBeforeRequest.addListener(
   ["blocking"]);
 
 
+//Blocking if images and stylesheets are the main document
+chrome.webRequest.onBeforeSendHeaders.addListener(
+  function (details) {
+    console.log(details.url)
+    return {
+      cancel: true
+    }
+  },
+  { urls: ["<all_urls>"], types: ['script', 'image', 'stylesheet'], tabId: this.tabId },
+  ["blocking"]);
+
+
 //OnInstall Boiler-Plate
 chrome.runtime.onInstalled.addListener(function () {
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
